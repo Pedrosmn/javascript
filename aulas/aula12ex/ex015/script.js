915a615825c408c0e472bf031cd97efd1881c3a9
Pedrosmn <128317304+Pedrosmn@users.noreply.github.com>
2023-06-13 12:10:00 -0300

    Exercício Detector de Idade

---
+++ aulas/aula12ex/ex015/script.js
@@ -0,0 +1,21 @@
+function verificar() {
+    var ano_nasc = document.querySelector('input#ano')
+    var res = document.querySelector('div#res')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var idade = ano - Number(ano_nasc.value)
+    if (ano_nasc.value.length == 0 || ano_nasc.value > ano) {
+        window.alert('ERRO, digite os dados corretamente!')
+    }
+    else {
+        var form_sex = document.getElementsByName('sex')
+        var genero = ''
+        if (form_sex[0].checked) {
+            genero = 'Homem'
+        }
+        else {
+            genero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos: ${genero} com ${idade} anos`
+    }
+}
