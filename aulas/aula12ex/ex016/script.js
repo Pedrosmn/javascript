54a4999cc6e6a5c97e29911cd929701327b0d761
Pedrosmn <128317304+Pedrosmn@users.noreply.github.com>
2023-06-16 16:30:46 -0300

    Finalização do exercício de repetição

--- aulas/aula12ex/ex016/script.js
+++ aulas/aula12ex/ex016/script.js
@@ -3,12 +3,24 @@ function contar() {
     var ifim = document.querySelector('input#fim')
     var ipasso = document.querySelector('input#passo')
     var res = document.querySelector('div#res')
-    var contador = Number(iinicio.value) + Number(ipasso.value)
 
-    for (var c = 1; c <= Number(ifim.value); c = c + Number(ipasso.value)) {
-        res.innerHTML = `${contador}`
-        contador = contador + Number(ipasso.value)
-        /*O res.innerHTML só está se substituindo, encontrar
-        um jeito de ir adicionando */
+    if (iinicio.value.length == 0 || ifim.value.length == 0 || ipasso.value.length == 0) {
+        window.alert('[ERRO] Digite todos os dados')
     }
+    else if (Number(iinicio.value) == Number(ifim.value)) {
+        window.alert('[ERRO] Impossível contar, os números são iguais.')
+    }
+    else if (Number(iinicio.value) >= Number(ifim.value)) {
+        res.innerHTML = `Contando: `
+        for (let c = Number(iinicio.value); c >= Number(ifim.value); c -= Number(ipasso.value))
+        res.innerHTML += `${c} > `
+    }
+    else {
+        res.innerHTML = `Contando: `
+        for (let c = Number(iinicio.value); c <= Number(ifim.value); c += Number(ipasso.value)) {
+        res.innerHTML += `${c} > `
+        }
+    }
+
+    
 }
